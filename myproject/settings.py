7a1a5c6986a328c7fd25e0a048fcee17236a494d
Filip <fkowalski858@gmail.com>
2024-07-16 15:43:59 +0200

    update for docker setting.

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,8 +20,9 @@ load_dotenv(dotenv_path=env_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.getenv("SECRET_KEY")
 # HOST_NAME = os.getenv("DJANGO_DATABASE_HOSTNAME")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
