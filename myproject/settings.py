a9826b51527608cb5a3f6f6d9806a40ed8678831
Filip <fkowalski858@gmail.com>
2024-07-16 16:25:28 +0200

    add cors in setting.

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,6 +79,34 @@ TEMPLATES = [
     },
 ]
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
